6d6417986cf315a16439e3c1d2f7f4824e58af98
At0micBee <hugo.vivien@protonmail.com>
2021-07-08 13:45:52 +0200

    Clean exercise 12

---
+++ 12_Working_with_data/changer.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+data = np.loadtxt("./N-BK7.csv", skiprows = 1, delimiter = ',')
+
+new = open("N-BK7.dat", "w+")
+
+new.write("{w:<12}{n:>17}{k:>17}\n".format(w = "Wavelength", n = 'n', k = 'k'))
+
+for i, wave in enumerate(data[:,0]):
+    new.write("{w:<12.4e}{n:>17.8e}{k:>17.8e}\n".format(w = wave, n = data[i,1], k = data[i,2]))
+
+new.close()
--- 12_Working_with_data/plotter.py
+++ 12_Working_with_data/plotter.py
@@ -11,15 +11,51 @@ pl.style.use("seaborn-paper")   # I just like this style
 
 ########################################################################################################################
 
-data = np.loadtxt("./data_1.dat")
+# Defining the function that computes the exit angle
+def get_angle(n1, n2, theta1):
+    return np.arcsin(n1 * np.sin(theta1) / n2)
 
-print( data[:,1] )
+########################################################################################################################
 
+# Loading the N-BK7 data from the file
+data = np.loadtxt("./N-BK7.dat", skiprows = 1)
+
+# Creating a figure to show n and k evolution
 pl.figure()
 
-pl.plot(data[:,0], data[:,1])
+pl.plot(data[:,0], data[:,1], color = "purple")
+
+pl.xlabel("Wavelength [$\lambda$]")
+pl.ylabel("n (purple)")
+pl.title("n, k indexes for N-BK7")
+
+pl.twinx()
+pl.plot(data[:,0], data[:,2], color = "teal")
+
+pl.ylabel("k (blue)")
 
 pl.savefig("data.pdf")
+pl.close()
+
+########################################################################################################################
+
+# Defining the incident angle and n1 value
+theta_incident = np.deg2rad(30)
+n_air = 1.0
+
+# Computing the exit angle of the beam depending on the wavelength
+exit_angle = get_angle(n_air, data[:,1], theta_incident)
+
+# Creating a figure to illustrate the variation
+pl.figure()
+
+pl.plot(data[:,0], np.rad2deg(exit_angle), color = "firebrick")
+
+pl.title(r"Exit angle ($n_{air}=1$, $\theta_1$ = 30°)")
+pl.xlabel("Wavelength [µm]")
+pl.ylabel("Angle [deg]")
+
+pl.savefig("exit_angle.pdf")
 pl.close()
 
 ########################################################################################################################
