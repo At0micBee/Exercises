b91dffea857256f984b02078636bfe5575b1153e
At0micBee <hugo.vivien@protonmail.com>
2021-07-16 13:54:13 +0200

    Added 17

---
+++ 17_Numpy_and_Pandas/comparison.py
@@ -0,0 +1,18 @@
+#
+# 17) Numpy and pandas
+#
+
+########################################################################################################################
+# Importing modules
+
+import os                       # OS operations
+import numpy as np              # Math and arrays
+import pandas as pd             # Dataframes functionalities
+import matplotlib.pyplot as pl  # Data visualization
+pl.style.use("seaborn-paper")   # I just like this style
+
+########################################################################################################################
+
+
+
+########################################################################################################################
